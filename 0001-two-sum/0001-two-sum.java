d501e02e23ea1a3b91d2c33fb6643f98d3db8b07
Ankit Kumar <97714826+ankit-kumar-developer-122@users.noreply.github.com>
2026-07-04 23:37:38 +0530

    Time: 134 ms (5.83%), Space: 47.1 MB (57.75%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        // This part is theoretically unreachable given the problem constraints
+        // that exactly one solution exists.
+        return new int[]{};
+    }
+}
