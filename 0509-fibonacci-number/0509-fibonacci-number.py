8844077fb615cd816b9e93f1739177e7ff18f617
Ankit Kumar <97714826+ankit-kumar-developer-122@users.noreply.github.com>
2026-07-21 01:13:49 +0530

    Time: 347 ms (5.06%), Space: 19.1 MB (56.83%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,9 +1,7 @@
 class Solution:
     def fib(self, n: int) -> int:
          return self.func(n)
-        #return ans
 
-    
     def func(self,num: int):
         if num == 0 or num == 1:
             return num
