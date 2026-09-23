89ea81952aa59bb70a420207df3a269f9942d9a3
Ankit Kumar <97714826+ankit-kumar-developer-122@users.noreply.github.com>
2026-09-23 23:49:09 +0530

    Time: 9 ms (45.81%), Space: 46.7 MB (93.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,29 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+        int n = nums.length;
+        int[][] pairs = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            pairs[i][0] = nums[i];
+            pairs[i][1] = i;
+        }
+        Arrays.sort(pairs, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int i = 0;
+        int j = n - 1;
+
+        while (i < j) {
+            int sum = pairs[i][0] + pairs[j][0];
+
+            if (sum == target) {
+                return new int[] { pairs[i][1], pairs[j][1] };
+            } else if (sum < target) {
+                i++;
+            } else {
+                j--;
             }
         }
-        // This part is theoretically unreachable given the problem constraints
-        // that exactly one solution exists.
-        return new int[]{};
+        return new int[] {};
     }
 }
